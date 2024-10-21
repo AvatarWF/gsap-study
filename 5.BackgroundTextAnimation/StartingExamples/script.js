0ed227f673197d8d39817e148d0e294d2dc12ea9
Fernando Filho <fernando_335@outlook.com>
2024-10-21 12:51:23 -0300

    Text with animated background

--- 5.BackgroundTextAnimation/StartingExamples/script.js
+++ 5.BackgroundTextAnimation/StartingExamples/script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    
+    var tl = gsap.timeline({repeat: -1, yoyo: true});
+
+    tl.to('h1', {
+      backgroundPosition: "0 -960px",
+      duration: 20,
+      ease: 'power.inOut'
+    })
 })
